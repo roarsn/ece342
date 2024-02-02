48059b65c659750d503dedf96890b42eaea2a87b
roarsn <aquamarineflow99@gmail.com>
2024-02-01 22:11:58 -0500

    added tabs

--- lab3-kit/lab3-kit/Core/Src/main.c
+++ lab3-kit/lab3-kit/Core/Src/main.c
@@ -35,14 +35,18 @@
 #define MAXIMUM 100000
 
 //Globals
-float* sin_LUT;
+float *sin_LUT;
 float *square_LUT;
 float *saw_LUT;
 float *triangle_LUT;
 
-double produceSin(double x);
+
 
 int8_t fs_terms=1; //make sure this works with xx_it extern var
+
+double produceSin(double x) {
+	return 1000 * sin(x) + 1100;
+}
 
 double squareWave(int max, double x){
   double fX = 0.5;
@@ -52,7 +56,7 @@ double squareWave(int max, double x){
   return fX;
 }
 double produceSquare(double x) {
-return 1000 * squareWave(10, x) + 1100;
+	return 1000 * squareWave(10, x) + 1100;
 }
 
 double sawTooth(int max, double x){
@@ -74,13 +78,12 @@ double triangleWave(int max, double x){
   return fX;
 }
 double produceTriangle(double x) {
-return 1000 * triangleWave(10, x) + 1100;
+	return 1000 * triangleWave(10, x) + 1100;
 }
 
 
-int main(void)
-{
-/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+int main(void) {
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* Configure the system clock */
@@ -91,87 +94,87 @@ int main(void)
   MX_DAC_Init();
   MX_TIM6_Init();
   MX_USART3_Init();
-HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-fs_terms=1;
+	HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	fs_terms=1;
 
-char message[100];
-  sprintf(message, "Printing test\n");
-  print_msg(message); //UART transmit
-
-
-HAL_DAC_Init(&hdac);
-  HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
-  HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 255);
-
-uint32_t sinoutput;
-
-//part 2.2 fill
-/*sin_LUT = malloc(sizeof(float) * 100);
-int i = 0;
-for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
-sin_LUT[i] = produceSin(rads);
-i++;
-}*/
-
-//part 3 - square
-/*square_LUT = malloc(sizeof(float) * 100);
-int i = 0;
-for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
-square_LUT[i] = produceSquare(rads);
-i++;
-}*/
-
-//part 3 - saw
-/*saw_LUT = malloc(sizeof(float) * 100);
-int i = 0;
-for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
-saw_LUT[i] = produceSaw(rads);
-i++;
-}*/
-
-//part 3 - triangle
-triangle_LUT = malloc(sizeof(float) * 100);
-int i = 0;
-for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
-triangle_LUT[i] = produceTriangle(rads);
-i++;
-}
+	char message[100];
+	sprintf(message, "Printing test\n");
+	print_msg(message); //UART transmit
 
 
-  while (1)
-  {
-//for part 1
-/*for (double x = 2*3.14; x < 4*3.14; x += 0.01) {
-double val = produceSin(x);
-HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, val);
-}*/
+	HAL_DAC_Init(&hdac);
+	HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+	HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 255);
 
-//for 2.2
-/*for (int i = 0; i < 100; i++) {
-HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, sin_LUT[i]);
-}*/
+	uint32_t sinoutput;
 
-//for 3 square
-/*for (int i = 0; i < 100; i++) {
-HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, square_LUT[i]);
-}*/
+	//part 2.2 
+	/*sin_LUT = malloc(sizeof(float) * 100);
+	int i = 0;
+	for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
+		sin_LUT[i] = produceSin(rads);
+		i++;
+	}*/
 
-//for 3 saw
-/*for (int i = 0; i < 100; i++) {
-HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, saw_LUT[i]);
-}*/
+	//part 3 - square
+	/*square_LUT = malloc(sizeof(float) * 100);
+	int i = 0;
+	for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
+		square_LUT[i] = produceSquare(rads);
+		i++;
+	}*/
 
-//for 3 triangle
-for (int i = 0; i < 100; i++) {
-HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, triangle_LUT[i]);
-}
-  }
-}
-#pragma GCC pop_options
+	//part 3 - saw
+	/*saw_LUT = malloc(sizeof(float) * 100);
+	int i = 0;
+	for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
+		saw_LUT[i] = produceSaw(rads);
+		i++;
+	}*/
 
-double produceSin(double x) {
-return 1000 * sin(x) + 1100;
-}
+	//part 3 - triangle
+	triangle_LUT = malloc(sizeof(float) * 100);
+	int i = 0;
+	for (double rads = 2*3.14; rads < 4*3.14; rads += (2.0*3.14/100.0)) {
+		triangle_LUT[i] = produceTriangle(rads);
+		i++;
+	}
+	
+	while (1)
+	{
+		//for part 1
+		/*for (double x = 2*3.14; x < 4*3.14; x += 0.01) {
+		double val = produceSin(x);
+			HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, val);
+		}*/
+
+		//for 2.2
+		/*for (int i = 0; i < 100; i++) {
+			HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, sin_LUT[i]);
+		}*/
+
+		//for 3 square
+		/*for (int i = 0; i < 100; i++) {
+			HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, square_LUT[i]);
+		}*/
+
+		//for 3 saw
+		/*for (int i = 0; i < 100; i++) {
+			HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, saw_LUT[i]);
+		}*/
+
+		//for 3 triangle
+		for (int i = 0; i < 100; i++) {
+			HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, triangle_LUT[i]);
+		}
+	}
+		
+				
+	}
+	#pragma GCC pop_options
+
+
+
 
 
 
