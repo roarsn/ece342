7343c3b6a106f3f3eae47bd9d89f81ecf7e82d1d
bijvoetr <129798717+bijvoetr@users.noreply.github.com>
2024-02-21 16:00:58 -0500

    starting lab 5

---
+++ lab5/Core/Src/main.c
@@ -0,0 +1,101 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+#include "config.h"
+#include "ov7670.h"
+
+
+/* USER CODE BEGIN PV */
+#define PREAMBLE "\r\n!START!\r\n"
+#define DELTA_PREAMBLE "\r\n!DELTA!\r\n"
+#define SUFFIX "!END!\r\n"
+
+uint16_t snapshot_buff[IMG_ROWS * IMG_COLS];
+uint8_t old_snapshot_buff[IMG_ROWS * IMG_COLS];
+
+uint8_t tx_buff[sizeof(PREAMBLE) + 2 * IMG_ROWS * IMG_COLS + sizeof(SUFFIX)];
+size_t tx_buff_len = 0;
+
+uint8_t dma_flag = 0;
+
+// Your function definitions here
+void print_buf(void);
+
+
+int main(void)
+{
+  /* Reset of all peripherals */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_DCMI_Init();
+  MX_USART3_UART_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_I2C2_Init();
+  MX_TIM1_Init();
+  MX_TIM6_Init();
+
+  char msg[100];
+
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  ov7670_init();
+  HAL_Delay(100);
+  ov7670_capture(snapshot_buff);
+
+  // Your startup code here
+  // Start the PWM timer for the camera clock
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+  // Initialize the OV7670 camera
+  ov7670_init();
+  // Additional delay to ensure camera module is ready after initialization
+  HAL_Delay(100); // 100 ms delay
+
+  // Optionally, send a message over UART to indicate the system is ready
+  char initMsg[] = "System Initialized and Ready\r\n";
+  HAL_UART_Transmit(&huart3, (uint8_t *)initMsg, strlen(initMsg), HAL_MAX_DELAY);
+  
+
+  while (1)
+  {
+    // Your code here
+    if (HAL_GPIO_ReadPin(USER_Btn_GPIO_Port, USER_Btn_Pin)) {
+      HAL_Delay(100);  // debounce
+
+      print_msg("Snap!\r\n");
+    }
+  }
+}
+
+
+void print_buf() {
+  // Send image data through serial port.
+  // Your code here
+}
