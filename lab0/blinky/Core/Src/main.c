6e60669fe8be321bc31eabe83f0e746ec280d587
bijvoetr <129798717+bijvoetr@users.noreply.github.com>
2024-04-01 22:50:32 -0400

    two street implementation

--- lab0/blinky/Core/Src/main.c
+++ lab0/blinky/Core/Src/main.c
@@ -20,22 +20,24 @@
 #include "config.h"
 
 typedef enum {
-LED_RED, // A0
-LED_BLUE, //A2
-LED_GREEN //A4
-} LED_Colour;
+  STATE_EAST_WEST_GREEN,
+  STATE_EAST_WEST_TRANSITION,  // East-West turns from green to red (through orange), North-South remains red
+  STATE_NORTH_SOUTH_GREEN,
+  STATE_NORTH_SOUTH_TRANSITION // North-South turns from green to red (through orange), East-West remains red
+} TrafficState;
 
 
 typedef struct State{
-LED_Colour activeLight;
-uint16_t time;
-struct State* next_state;
+  TrafficState trafficState;
+  uint16_t time;
+  struct State* next_state;
 } State;
 
 //define states
-State redState = {LED_RED, (uint16_t)3000, NULL};
-State blueState = {LED_BLUE, (uint16_t)1000, NULL};
-State greenState = {LED_GREEN, (uint16_t)3000, NULL};
+State east_west_green = {STATE_EAST_WEST_GREEN, 3000, NULL};
+State east_west_transition = {STATE_EAST_WEST_TRANSITION, 1000, NULL};
+State north_south_green = {STATE_NORTH_SOUTH_GREEN, 3000, NULL};
+State north_south_transition = {STATE_NORTH_SOUTH_TRANSITION, 1000, NULL};
 
 
 int main(void)
@@ -54,35 +56,61 @@ int main(void)
 
 
 // ink states into a loop
-redState.next_state = &blueState;
-blueState.next_state = &greenState;
-greenState.next_state = &redState;
+east_west_green.next_state = &east_west_transition;
+east_west_transition.next_state = &north_south_green;
+north_south_green.next_state = &north_south_transition;
+north_south_transition.next_state = &east_west_green;
 
 //Set initial state
-State* current_state = &redState;
+State* current_state = &east_west_green;
 
   /* Infinite loop */
   while (1)
   {
 
-/*switch(current_state->activeLight){
+  //for EW, pin A0 = Red, A1 = Orange, A2 = Green
+  //for NS, pin A3 = Red, A4 = Orange, A5 = Green
+  switch(current_state->trafficState) {
+    case STATE_EAST_WEST_GREEN:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_RESET); 
+      break;
+    case STATE_EAST_WEST_TRANSITION:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_3, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_RESET); 
+      break;
+    case STATE_NORTH_SOUTH_GREEN:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_5, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4, GPIO_PIN_RESET); 
+      break;
+    case STATE_NORTH_SOUTH_TRANSITION:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_4, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_5, GPIO_PIN_RESET); 
+      break;
 
-case LED_RED:
-HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);   // Red ON
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_4, GPIO_PIN_RESET); // Blue & Green OFF
-break;
-case LED_BLUE:
-HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);   // Red ON
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_4, GPIO_PIN_RESET); // Blue & Green OFF
-break;
-case LED_GREEN:
-HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);   // Red ON
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_0, GPIO_PIN_RESET); // Blue & Green OFF
-break;
+
+
+
+  }
 }
-*/
 
-switch(current_state->activeLight){
+//test pins
+//HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0); // Toggle A0 LED
+    //HAL_Delay(1000); // 1-second delay
+
+//For blinking board
+    //HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+//HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+ //HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    //HAL_Delay(500);
+
+//For blinking board
+    //HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    //HAL_Delay(500);
+
+
+//for single traffic light on board
+/* switch(current_state->activeLight){
 
 case LED_RED:
 HAL_GPIO_WritePin(GPIOB, LD1_Pin, GPIO_PIN_SET);   // Red ON
@@ -98,23 +126,18 @@ HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_SET);   // Red ON
 break;
 }
 HAL_Delay(current_state -> time);
-current_state = current_state -> next_state;
+current_state = current_state -> next_state; */
 
-
-
-//test pins
-//HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0); // Toggle A0 LED
-    //HAL_Delay(1000); // 1-second delay
-
-//For blinking board
-    //HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
-//HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
- //HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-    //HAL_Delay(500);
-
-  }
-}
-
-//For blinking board
-    //HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-    //HAL_Delay(500);
+//     case LED_RED:
+//       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);   // Red ON
+//       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_4, GPIO_PIN_RESET); // Blue & Green OFF
+//       break;
+//     case LED_BLUE:
+//       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);   // Red ON
+//       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_4, GPIO_PIN_RESET); // Blue & Green OFF
+//       break;
+//     case LED_GREEN:
+//       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);   // Red ON
+//       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_0, GPIO_PIN_RESET); // Blue & Green OFF
+//       break;
+// }
